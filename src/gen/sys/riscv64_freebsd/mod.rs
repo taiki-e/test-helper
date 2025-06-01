415db36f17de0c3e940d3cdf657053844b98a11c
Taiki Endo <te316e89@gmail.com>
2025-06-01 02:54:59 +0000

    codegen: Update src/gen/sys

--- src/gen/sys/riscv64_freebsd/mod.rs
+++ src/gen/sys/riscv64_freebsd/mod.rs
@@ -440,6 +440,7 @@ pub use self::sys_syscall::SYS_kcmp;
 pub use self::sys_syscall::SYS_getrlimitusage;
 pub use self::sys_syscall::SYS_fchroot;
 pub use self::sys_syscall::SYS_setcred;
+pub use self::sys_syscall::SYS_exterrctl;
 pub use self::sys_syscall::SYS_MAXSYSCALL;
 mod sys_sysctl;
 pub use self::sys_sysctl::CTL_KERN;
--- src/gen/sys/riscv64_freebsd/sys_syscall.rs
+++ src/gen/sys/riscv64_freebsd/sys_syscall.rs
@@ -435,4 +435,5 @@ pub const SYS_kcmp: u32 = 588;
 pub const SYS_getrlimitusage: u32 = 589;
 pub const SYS_fchroot: u32 = 590;
 pub const SYS_setcred: u32 = 591;
-pub const SYS_MAXSYSCALL: u32 = 592;
+pub const SYS_exterrctl: u32 = 592;
+pub const SYS_MAXSYSCALL: u32 = 593;
