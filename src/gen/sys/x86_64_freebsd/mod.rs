87a3c03e408f28354641eca3beaa762a8de93f4f
Taiki Endo <te316e89@gmail.com>
2026-03-06 03:09:33 +0000

    codegen: Update src/gen/sys

--- src/gen/sys/x86_64_freebsd/mod.rs
+++ src/gen/sys/x86_64_freebsd/mod.rs
@@ -493,6 +493,7 @@ pub use self::sys_syscall::SYS_jail_remove_jd;
 pub use self::sys_syscall::SYS_kexec_load;
 pub use self::sys_syscall::SYS_pdrfork;
 pub use self::sys_syscall::SYS_pdwait;
+pub use self::sys_syscall::SYS_renameat2;
 pub use self::sys_syscall::SYS_MAXSYSCALL;
 mod sys_sysctl;
 pub use self::sys_sysctl::CTL_KERN;
